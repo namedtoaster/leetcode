bc4d5c83e7875d07d2b4187d371ba4be6f4f0865
Dave Campbell <davecampbell@Daves-MacBook-Pro.local>
2023-01-18 17:22:52 -0800

    add script to create new cpp file from template

---
+++ scripts/new_problem.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp scripts/template.cpp $1
---
+++ scripts/template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int problemFunction() {
+  }
+};
+
+int main() {
+  vector<> tests;
+
+  tests.push_back();
+
+  Solution sol;
+  for ( : tests) {
+    cout << sol.problemFunction() << endl;
+  }
+}
